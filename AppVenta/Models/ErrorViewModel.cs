6ecb7b909b42b63125511b27f2d8657bad933d0b
gerson7777 <gersonbreakgonzalez@gmail.com>
2020-04-25 13:51:49 -0600

    Agregar archivos de proyecto.

---
+++ AppVenta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppVenta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
